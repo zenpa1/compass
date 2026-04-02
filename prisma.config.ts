24837af11c934a63ed58b96d62139c0ca2c1d53b
hammmwebv <hamilcar.gisala.cics@ust.edu.ph>
2026-04-02 15:25:50 +0800

    Update datasource to use PrismaMariaDb adapter

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,6 +9,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    adapter: new PrismaMariaDb(process.env.DATABASE_URL!),
   },
 });
