ebec1a53e4cf86dc07de7fdf4fbb592346d2db5b
Ham <hamilcar.gisala.cics@ust.edu.ph>
2026-04-03 12:07:46 +0800

    changed command

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
+    seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
     url: process.env.DATABASE_URL!,
