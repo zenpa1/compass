042922fe2693a76ffdc9eeab32b610bef01dc1a7
Ham <hamilcar.gisala.cics@ust.edu.ph>
2026-05-02 16:27:42 +0800

    updated page

--- src/app/(auth)/invite/page.tsx
+++ src/app/(auth)/invite/page.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { Suspense } from "react";
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export default function InvitePage() {
+function InviteContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -16,4 +17,12 @@ export default function InvitePage() {
   }, []);
 
   return <p>Activating your account...</p>;
+}
+
+export default function InvitePage() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <InviteContent />
+    </Suspense>
+  );
 }
